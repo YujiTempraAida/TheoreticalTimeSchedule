d91ef20b34d7adcff8234d97f9f0c3a0fa67457f
YujiAIda <tempra.ssb4@gmail.com>
2023-09-18 01:29:28 +0900

    deleted unused import

--- src/components/LeftAlignedTimeline.tsx
+++ src/components/LeftAlignedTimeline.tsx
@@ -1,4 +1,3 @@
-import {useState} from 'react'
 import Timeline from '@mui/lab/Timeline';
 import TimelineItem from '@mui/lab/TimelineItem';
 import TimelineSeparator from '@mui/lab/TimelineSeparator';
